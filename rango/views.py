c672713a75bdb11628fff81c5746674f80da55d7
Julia Someya <2246746s@student.gla.ac.uk>
2018-01-31 12:42:58 +0000

    up to chapter 6, all working, without chapter 6 exercise

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -10,5 +11,20 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': "Cookies!"}
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+    
 
 
