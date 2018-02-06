aaa6ff88e5e65d68ebcd46426368d417fea73735
Julia Someya <2246746s@student.gla.ac.uk>
2018-02-06 13:43:05 +0000

    up to ch9 & ch9 exercise

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -31,3 +32,14 @@ class Page(models.Model):
 
 class PageAdmin(admin.ModelAdmin):
     list_display = {'title','category','url'}
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length':6},
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -108,6 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.PBKDF2PasswordHasher',
+                    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher'
+                    )
 
 
 # Internationalization
@@ -129,3 +135,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+LOGIN_URL = '/rango/login/'
